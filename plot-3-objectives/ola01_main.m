58846f4b681be29cef068b834ebea91d9abb1408
Peerasak Wangsom <pwangsom@gmail.com>
2018-10-31 01:19:08 +0700

    Commit ola05

--- plot-3-objectives/ola01_main.m
+++ plot-3-objectives/ola01_main.m
@@ -4,14 +4,14 @@ close all;
 
 file_filters = ...
 [
-"250_08";
-"500_13";
-"750_12";
+"250_13";
+"500_09";
+"750_04";
 "1000_08";
 ];
 
-source_folder_name = 'ola04/out/';
-dest_folder_name = 'ola04/pics/';
+source_folder_name = 'ola05/out/';
+dest_folder_name = 'ola05/pics/';
 
 for n = 1 : length(file_filters)
         
